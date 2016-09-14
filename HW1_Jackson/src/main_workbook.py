c2218c03ae2ba2722207309daf61465f7cd13914
Kyle Jackson <k.jackson0794@gmail.com>
2016-09-14 16:52:12 -0400

    Add files via upload

--- HW1_Jackson/src/main_workbook.py
+++ HW1_Jackson/src/main_workbook.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import nltk
+import re
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import linear_kernel
@@ -33,18 +34,49 @@ def preprocessing(raw_text_df):
     """ 
     preprocesses a DataFrame vector containing text
     """
+    
     stemmer = nltk.stem.porter.PorterStemmer()
     tokenizer = RegexpTokenizer(r'\w+')
     # iterate over all lines for preprocessing
     for index, line in enumerate(raw_text_df):
+        
+        # if there is mention of stars from 1-5, change the integer into
+        # text and combine the number and the word "star" to make a new word
+        # example: "I give this product 1 star" is now "I give this product onestar"
+        # why? numbers are removed as part of preprocessing
+        if "1 star" in line:
+            line = line.replace("1 star", "onestar")
+        if "1 stars" in line:
+            line = line.replace("1 stars", "onestar")
+        if "2 star" in line:
+            line = line.replace("2 star", "twostars")
+        if "2 stars" in line:
+            line = line.replace("2 stars", "twostars")
+        if "3 star" in line:
+            line = line.replace("3 star", "threestars")
+        if "3 stars" in line:
+            line = line.replace("3 stars", "threestars")
+        if "4 star" in line:
+            line = line.replace("4 star", "fourstars")
+        if "4 stars" in line:
+            line = line.replace("4 stars", "fourstars")
+        if "5 star" in line:
+            line = line.replace("5 star", "fivestars")
+        if "5 stars" in line:
+            line = line.replace("5 stars", "fivestars")
+        
         # tokenize lines
-        tokens = tokenizer.tokenize(line)
+        tokens = re.split('(\d+)',line)
+        # remove numbers
+        no_digits = [w for w in tokens if not w.isdigit()]
+        # join tokens
+        joined_text = " ".join(no_digits)
+        # re tokenize
+        tokens = tokenizer.tokenize(joined_text)
         # make tokens lowercase
         lower_tokens = [w.lower() for w in tokens if type(w) == str] 
-        # remove numbers
-        no_digits = [w for w in lower_tokens if not w.isdigit()]
         # remove stopwords
-        stopped_tokens = [w for w in no_digits if not w in stopwords.words('english')]
+        stopped_tokens = [w for w in lower_tokens if not w in stopwords.words('english')]
         # stem words
         clean_tokens = [stemmer.stem(w) for w in stopped_tokens]
         # join text
@@ -53,6 +85,7 @@ def preprocessing(raw_text_df):
         raw_text_df[index] = joined_text
         print(index)
 
+# preprocessing raw_text
 preprocessing(train_X)
 preprocessing(test_X)
 
@@ -63,7 +96,7 @@ test_tfidf = vectorizer.transform(test_X)
 
 ########################################################################  
 
-def kNN(k, test_file, train_file):
+def kNN(k, testTfidf, trainTfidf, test_file, train_label, weight=True):
     """
     implementation of k nearest neighbor algorithm using cosine similarity
     
@@ -73,41 +106,68 @@ def kNN(k, test_file, train_file):
     k: integer
     number of nearest neighbors used for classification
     
-    test_tfidf: sparse matrix
+    testTfidf: sparse matrix
     
-    train_tfidf: sparse matrix
+    trainTfidf: sparse matrix
+    
+    test_file:
+    contains the preprocessed text for the kNN method to classify
+    
+    train_label: 
+    contains the labels from the training set
+    
+    weight: boolean
+    default True. Uses simple weight to calculate classification
     
     """
+    test_y = []    
     
     # iterate through all lines in the test reviews and classify them
-    for index, line in enumerate(test_X):
+    for index, line in enumerate(test_file):
         # cosine similarity
-        cos_similarity = linear_kernel(test_file[index:index+1], train_file).flatten()
-        # get the indices of nearest neighbors based on k parameter        
-        neighbor_indices = cos_similarity.argsort()[:-k:-1]
-        # get a list of labels from the neighbors and sum the list
-        labels_list = train_y[neighbor_indices].tolist()
-        label_sum = sum(labels_list)
+        cos_similarity = linear_kernel(testTfidf[index:index+1], trainTfidf).flatten()
         
-        # if the label is 0 (tie between +1 and -1) use k-1 neighbors
-        if label_sum == 0:
-            kNN(k-1, test_file, train_file)
-            
-        # classify based on majority
-        elif label_sum > 0:
-            test_y.append("+1")
+        if weight == True:
+            # get the indices of nearest neighbors based on k parameter        
+            neighbor_indices = cos_similarity.argsort()[:-k:-1]
+            # similarities
+            similarities = cos_similarity[neighbor_indices]
+            # get a list of labels from the neighbors and sum the list
+            labels_list = train_label[neighbor_indices].tolist()
+
+            my_list = []            
+            for s, l in zip(similarities, labels_list):
+                if l == -1:
+                    my_list.append(-s)
+                else:
+                    my_list.append(s)   
+                    
+            label_sum = sum(my_list)
+            if label_sum > 0:
+                test_y.append("+1")
+            else:
+                test_y.append(-1)
+
         else:
-            test_y.append(-1)
-            
+            # get the indices of nearest neighbors based on k parameter        
+            neighbor_indices = cos_similarity.argsort()[:-k:-1]
+            # get a list of labels from the neighbors and sum the list
+            labels_list = train_label[neighbor_indices].tolist()
+            label_sum = sum(labels_list)
+
+            if label_sum > 0:
+                test_y.append("+1")
+            else:
+                test_y.append(-1)
+                
         print(index)
             
-    return pd.DataFrame(test_y)       
+    return pd.DataFrame(test_y)   
 
 #########################################################################
 
-test_y = []
-submission = kNN(k=20, test_file=test_tfidf, train_file=train_tfidf)
+submission = kNN(k=20, testTfidf=test_tfidf, trainTfidf=train_tfidf, test_file=test_X, train_label=train_y, weight=True)
 
 # writing to .txt
-np.savetxt(r'submissions/submission1.txt', submission.values, fmt='%s')
+np.savetxt(r'submissions/submission8-weight-k20-star.txt', submission.values, fmt='%s')
 
---
+++ HW1_Jackson/src/validation_workbook.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+from sklearn import cross_validation
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+
+os.chdir("C:/Users/Kyle/OneDrive/Documents/GMU Classes/CS 584/HW1_Jackson")
+
+# reading in test and train
+train = pd.read_table("data/train.data", header=None, skip_blank_lines=False)
+
+# renaming column headers
+train.columns = ["target", "data"]
+
+# drop missing text labels in train to reduce noise
+train = train.dropna()
+train = train.reset_index(drop=True)
+
+preprocessing(train.data)
+
+# split text and target
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(train.data, train.target, test_size=0.2, random_state=0)
+
+y_test = pd.DataFrame(y_test).reset_index(drop=True)
+
+
+#########################################################################
+
+# create tf-idf matrix for train and test
+vectorizer = TfidfVectorizer()
+train_tfidf = vectorizer.fit_transform(X_train)
+test_tfidf = vectorizer.transform(X_test)
+
+########################################################################  
+
+# this kNN implementation is modified for cross_validation. See main_workbook.py
+# for my kNN implementation
+def kNN(k, testTfidf, trainTfidf, test_file, train_label, weight=True):
+    test_y = []    
+    # iterate through all lines in the test reviews and classify them
+    for index, line in enumerate(test_file):
+        # cosine similarity
+        cos_similarity = linear_kernel(testTfidf[index:index+1], trainTfidf).flatten()
+        
+        
+        if weight == True:
+            # get the indices of nearest neighbors based on k parameter        
+            neighbor_indices = cos_similarity.argsort()[:-k:-1]
+            # similarities
+            similarities = cos_similarity[neighbor_indices]
+            # get a list of labels from the neighbors and sum the list
+            labels_list = train_label[neighbor_indices].tolist()
+
+            my_list = []            
+            for s, l in zip(similarities, labels_list):
+                if l == -1:
+                    my_list.append(-s)
+                else:
+                    my_list.append(s)
+            
+            label_sum = sum(my_list)
+
+            if label_sum > 0:
+                test_y.append(1)
+            else:
+                test_y.append(-1)
+
+        else:
+            # get the indices of nearest neighbors based on k parameter        
+            neighbor_indices = cos_similarity.argsort()[:-k:-1]
+            # get a list of labels from the neighbors and sum the list
+            labels_list = train_label[neighbor_indices].tolist()
+            label_sum = sum(labels_list)
+
+            if label_sum > 0:
+                test_y.append(1)
+            else:
+                test_y.append(-1)
+                
+    return pd.DataFrame(test_y)   
+
+
+#########################################################################
+
+# for loop iterates through possible values for k
+k_list = []
+weight_score_list = []
+non_weight_score_list = []
+for i in [5,7,9,11,13,15,17,19,20,21,23,25,27,29]:
+    
+    # weighted validation
+    submission = kNN(k=i, testTfidf=test_tfidf, trainTfidf=train_tfidf, test_file=X_test, train_label=y_train, weight=True)    
+    weight_df = pd.concat([submission, y_test], axis=1)
+    weight_df.columns = ["pred","actual"]
+    weight_df["score"] = 0
+    weight_df["score"] = np.where(weight_df['pred'] == weight_df['actual'], 1, 0)         
+        
+    # not weighted validation
+    submission1 = kNN(k=i, testTfidf=test_tfidf, trainTfidf=train_tfidf, test_file=X_test, train_label=y_train, weight=False)
+    non_weight_df = pd.concat([submission1, y_test], axis=1)
+    non_weight_df.columns = ["pred","actual"]
+    non_weight_df["score"] = 0
+    non_weight_df["score"] = np.where(non_weight_df['pred'] == non_weight_df['actual'], 1, 0)
+
+    # compute score
+    weight_score = sum(weight_df.score)/3700  
+    non_weight_score = sum(non_weight_df.score)/3700
+    
+    print("With weight and k=", i, ", score is", weight_score)
+    print("Without weight and k=", i, ", score is", non_weight_score)
+    
+    # create lists containing the scores for making a graphic
+    
+    k_list.append(i)    
+    weight_score_list.append(weight_score)
+    non_weight_score_list.append(weight_score)
+
+
+
+
+
+
